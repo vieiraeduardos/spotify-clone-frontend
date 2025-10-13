7c8dfcccaab782423db489bf0e42236abad729a8
Eduardo Vieira <edusvieirap@gmail.com>
2025-10-13 00:06:33 -0300

    Feat: Implementar menu lateral e fluxo de roteamento com React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Artists from "./components/Artists";
+import Playlists from "./components/Playlists";
+import Albums from "./components/Albums";
+import Profile from "./components/Profile";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="container">
+
+        <BrowserRouter>
+          <Sidebar />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/artists" element={<Artists />} />
+              <Route path="/playlists" element={<Playlists />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/artists/:id" element={<Albums />} />
+            </Routes>
+          </main>
+        </BrowserRouter>
+      </div >
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+
+import spotifyLogo from "../assets/spotify-logo.svg";
+import homeIcon from "../assets/home-icon.svg";
+import discIcon from "../assets/disc-icon.svg";
+import playIcon from "../assets/play-icon.svg";
+import userIcon from "../assets/user-icon.svg";
+import installIcon from "../assets/install-icon.svg";
+
+import "./Sidebar.css";
+
+export default function Sidebar() {
+    return (
+        <nav className="sidebar">
+
+            <div className="nav-logo">
+                <img src={spotifyLogo} className="logo" alt="Spotify logo" width={164} height={49.06} />
+            </div>
+
+            <Link to="/">
+                <div className="nav-option">
+                    <img src={homeIcon} alt="Início" />
+                    <p>Home</p>
+                </div>
+            </Link>
+
+            <Link to="/artistas">
+                <div className="nav-option">
+                    <img src={discIcon} alt="Artistas" />
+                    <p>Artistas</p>
+                </div>
+            </Link>
+
+            <Link to="/playlists">
+                <div className="nav-option">
+                    <img src={playIcon} alt="Playlists" />
+                    <p>Playlists</p>
+                </div>
+            </Link>
+
+            <Link to="/perfil">
+                <div className="nav-option">
+                    <img src={userIcon} alt="Perfil" />
+                    <p>Perfil</p>
+                </div>
+            </Link>
+
+            <div className="nav-option">
+                <img src={installIcon} alt="Instalar PWA" />
+                <p>Instalar PWA</p>
+            </div>
+        </nav>
+    );
+}

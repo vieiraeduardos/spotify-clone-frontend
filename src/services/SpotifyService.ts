06c6a3025b20793b3acc631c7b690ff482d00e02
Eduardo Vieira <edusvieirap@gmail.com>
2025-10-14 09:59:05 -0300

    Fix: Tornar váriavel 'possible' uma constante

--- src/services/SpotifyService.ts
+++ src/services/SpotifyService.ts
@@ -50,7 +50,7 @@ export default class SpotifyService {
 
     private generateCodeVerifier(length: number) {
         let text = "";
-        let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
         for (let i = 0; i < length; i++) {
             text += possible.charAt(Math.floor(Math.random() * possible.length));

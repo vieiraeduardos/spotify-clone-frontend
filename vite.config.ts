21a7023b98118bc855082659ab711dc9e1f4d5bd
Eduardo Vieira <edusvieirap@gmail.com>
2025-10-15 08:22:44 -0300

    Feat: Integrar Sentry na aplicação

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest" />
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -6,81 +7,85 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
-        type: 'module'
-      },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg'],
-      manifest: {
-        name: 'Luiza Labs - Spotify Clone',
-        short_name: 'Luiza Labs',
-        description: 'Projeto de integração com a API do Spotify - Clone do Spotify',
-        theme_color: '#1DB954',
-        background_color: '#000000',
-        display: 'standalone',
-        start_url: '/',
-        scope: '/',
-        icons: [
-          {
-            src: '/icons/icon-128x128.png',
-            sizes: '128x128',
-            type: 'image/png'
-          },
-          {
-            src: '/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/api\.spotify\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'spotify-api-cache',
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 1 // 1 hora
-              }
-            }
-          },
-          {
-            urlPattern: /^https:\/\/i\.scdn\.co\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'spotify-images-cache',
-              expiration: {
-                maxEntries: 500,
-                maxAgeSeconds: 60 * 60 * 1 // 1 hora
-              }
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true,
+      type: 'module'
+    },
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg'],
+    manifest: {
+      name: 'Luiza Labs - Spotify Clone',
+      short_name: 'Luiza Labs',
+      description: 'Projeto de integração com a API do Spotify - Clone do Spotify',
+      theme_color: '#1DB954',
+      background_color: '#000000',
+      display: 'standalone',
+      start_url: '/',
+      scope: '/',
+      icons: [
+        {
+          src: '/icons/icon-128x128.png',
+          sizes: '128x128',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: '/icons/icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/api\.spotify\.com\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'spotify-api-cache',
+            expiration: {
+              maxEntries: 100,
+              maxAgeSeconds: 60 * 60 * 1 // 1 hora
             }
           }
-        ]
-      }
-    })
-  ],
+        },
+        {
+          urlPattern: /^https:\/\/i\.scdn\.co\/.*/i,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'spotify-images-cache',
+            expiration: {
+              maxEntries: 500,
+              maxAgeSeconds: 60 * 60 * 1 // 1 hora
+            }
+          }
+        }
+      ]
+    }
+  }), sentryVitePlugin({
+    org: "accenture-99c",
+    project: "luiza-labs"
+  })],
+
   server: {
     port: 8080,
     host: "0.0.0.0",
     allowedHosts: [".onrender.com"]
   },
+
   preview: {
     port: 8080,
     host: "0.0.0.0",
     allowedHosts: [".onrender.com"]
   },
+
   test: {
     coverage: {
       provider: 'v8', 
@@ -88,4 +93,8 @@ export default defineConfig({
       reportsDirectory: './coverage',
     },
   },
+
+  build: {
+    sourcemap: true
+  }
 })
